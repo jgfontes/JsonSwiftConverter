dd586a3a6f302c46d239bf3691388d29d5e77428
Julio Conta Pessoal <ju.1996@outlook.com>
2022-11-20 17:13:25 -0300

    Converter from Swift to Json partially working. Some fix need in the values section

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,32 @@ import java.util.TreeMap;
 public class Main {
     public static void main(String[] args) {
         //Declaring varaibles
+//        String swiftStr = """
+//        - 1 : "name: Optional(\"Julio\")"
+//        - 2 : "age: Optional(\"25\")"
+//        - 2 : "age: Optional(\"26\")"
+//        - 2 : "age: Optional(\"27\")"
+//        - 3 : "person.location.state: Optional(\"NY\")"
+//        - 4 : "person.location.country: Optional(\"US\")"
+//        - 5 : "comment: Optional(\"Nice guy\")"
+//                """;
+//
         String swiftStr = """
-        - 1 : "name: Optional(\"Julio\")"
-        - 2 : "age: Optional(\"25\")"
-        - 2 : "age: Optional(\"26\")"
-        - 2 : "age: Optional(\"27\")"
-        - 3 : "person.location.state: Optional(\"NY\")"
-        - 4 : "person.location.country: Optional(\"US\")"
-        - 5 : "comment: Optional(\"Nice guy\")"
+        - 1 : "name.aa.cc: Optional(\"Julio1\")"
+        - 1 : "name.aa.cc: Optional(\"Julio2\")"
+        - 1 : "name.bb.aa: Optional(\"Julio3\")"
+        - 1 : "name.b.bb: Optional(\"Julio4\")"
+        - 1 : "name.b.x: Optional(\"Julio5\")"
+        - 1 : "name.b.y: Optional(\"Julio6\")"
+        - 1 : "name.a.b.c.d.e.f.g: Optional(\"Julio7\")"
+        - 2 : "age.uuuu: Optional(\"21\")"
+        - 2 : "age.aa: Optional(\"20\")"
+        - 2 : "age.bb: Optional(\"25\")"
+        - 2 : "age.bb.cc: Optional(\"41\")"
                 """;
 
         TreeMap swiftHashMap = swiftToJson.convertFromSwiftToTreeMap(swiftStr);
-        System.out.println(swiftHashMap);
+        System.out.println(swiftToJson.convertFromTreeMapToJson(swiftHashMap));
 
-        System.out.println(swiftHashMap.containsKey("age"));
     }
 }
--- src/converters/swiftToJson.java
+++ src/converters/swiftToJson.java
@@ -1,8 +1,6 @@
 package converters;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -26,7 +24,73 @@ public class swiftToJson {
         return conversionMap;
     }
 
-//    public static String convertFromTreeMapToJson(TreeMap<String, String> treeMapConverted) {
-//
-//    }
+    public static String convertFromTreeMapToJson(TreeMap<String, ArrayList<String>> treeMapConverted) {
+        //Setting entries and final String
+        Set<Map.Entry<String, ArrayList<String>>> entries = treeMapConverted.entrySet();
+        ArrayList<String> values = new ArrayList<String>();
+        String jsonString = "{\n";
+        System.out.println(entries);
+
+        //Declaring variables using in the comparison of the elements
+        String[] lastIterationKeys = null;
+        int equalityOrder = 0;
+        int sizeLastIteration = 0;
+        int sizeThisIteration = 0;
+
+        for (Map.Entry<String, ArrayList<String>>entry : entries) {
+            //Validate and add the key
+            String[] keys = entry.getKey().split("\\.");
+            sizeThisIteration = keys.length;
+
+            //Setting the comparison variables
+            equalityOrder = setEqualityOrder(keys, lastIterationKeys);
+
+            //PRINTING FOR DEBUGGING PURPOSE
+            System.out.println("equality Order: " + equalityOrder);
+            System.out.println("sizeThisIter: " + sizeThisIteration);
+            System.out.println("sizeLastIter: " + sizeLastIteration);
+
+            //Logic for closing curly brackets
+            for(int i = 0; i < (sizeLastIteration - equalityOrder); i++) {
+                jsonString = jsonString.concat("},");
+            }
+
+            //Logic for opening curly brackets
+            for (int i = 0; i < (sizeThisIteration - equalityOrder); i++) {
+                jsonString = jsonString.concat("\"" + keys[i + equalityOrder] + "\": {");
+            }
+            jsonString = jsonString.concat("\n");
+            System.out.println(jsonString + "\n\n");
+
+            //Logic for adding the values in the Json
+            for(String value: entry.getValue()) {
+                jsonString = jsonString.concat("\"" + value + "\",");
+            }
+
+            //Prepare variables for next loop
+            lastIterationKeys = keys;
+            sizeLastIteration = keys.length;
+        };
+        //Logic for closing curly brackets
+        while(sizeLastIteration > 0) {
+            jsonString = jsonString.concat("}");
+            sizeLastIteration --;
+        }
+
+        return jsonString;
+    }
+
+    private static int setEqualityOrder(String[] keys, String[] lastIterationKeys) {
+        if(lastIterationKeys == null) {
+            return 0;
+        }
+
+        int equalityOrder = 0;
+        for (equalityOrder = 0; equalityOrder < Integer.min(keys.length, lastIterationKeys.length); equalityOrder ++) {
+            if (!keys[equalityOrder].equals(lastIterationKeys[equalityOrder])) {
+                break;
+            }
+        }
+        return equalityOrder;
+    }
 }
